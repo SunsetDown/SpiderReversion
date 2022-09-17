e36db929df86d32f2526d6659040bca29138c557
SusetDown <skyamerica@outlook.com>
2022-09-17 17:54:30 +0800

    bing翻译爬虫

--- BingTranslate.py
+++ BingTranslate.py
@@ -1,10 +1,14 @@
 import requests
 import re
+from fake_useragent import UserAgent as ua
 
 
 class Bing:
     session = requests.Session()
     times = 1
+    headers = {
+        'user-agent': ua().random
+    }
 
     def __init__(self, text: str) -> None:
         self.text = text
@@ -16,13 +20,10 @@ class Bing:
 
     def get_token(self):
         url = 'https://cn.bing.com/translator?ref=TThis&from=en&to=zh-Hans&isTTRefreshQuery=1'
-        headers = {
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.33'
-        }
 
         pattren = re.compile(r'params_RichTranslateHelper = \[(.*?)\];')
 
-        resp = self.session.get(url, headers=headers)
+        resp = self.session.get(url, headers=self.headers)
         res = pattren.findall(resp.text)[0]
 
         return self.deal_token(res)
@@ -30,9 +31,6 @@ class Bing:
     def bing_translate(self):
         url = f'https://cn.bing.com/ttranslatev3?isVertical=1&&IG=1B558B84854143D69302D7F8575CF95E&IID=translator.5022.{self.times}'
 
-        headers = {
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.33'
-        }
 
         data = {
             # '': '',
@@ -42,7 +40,7 @@ class Bing:
         }
 
         data['key'], data['token'] = self.get_token()
-        resp = self.session.post(url, data=data, headers=headers)
+        resp = self.session.post(url, data=data, headers=self.headers)
         self.times += 2
         if resp.status_code != 200:
             raise ValueError(data)
