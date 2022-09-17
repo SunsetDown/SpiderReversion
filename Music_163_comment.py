f227e0c051c07bff60b617bc6f34d384fe7ba2bf
SusetDown <skyamerica@outlook.com>
2022-09-17 22:37:16 +0800

    将加密模块外置

--- Music_163_comment.py
+++ Music_163_comment.py
@@ -2,16 +2,7 @@ import requests
 import execjs
 from time import time, sleep
 import json
-from pprint import pprint
-
-
-def get_param(d,
-              e="010001",
-              f="00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7",
-              g="0CoJUm6Qyw8W8jud"):
-    with open('SpiderReversion/Music_163.js', 'r', encoding='utf-8')as file:
-        ctx = execjs.compile(file.read())
-    return ctx.call('copy', d, e, f, g)
+from SpiderReversion.Music_163_params import get_param
 
 
 def deal_resp(data: dict):
